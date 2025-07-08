c9bc8e62e13641ebe453c1b4389b79f9309e6219
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-07-08 21:22:51 +0300

    Update webpack.config.js

--- 0x04-TypeScript/task_5/js/webpack.config.js
+++ 0x04-TypeScript/task_5/js/webpack.config.js
@@ -1,1 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/main.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  mode: 'development'
+};
 
