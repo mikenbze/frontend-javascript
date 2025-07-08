6c0921f405c75f9d3777fa737362ca4a0f2c8aee
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-07-08 21:03:35 +0300

    Update Java.ts

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,1 +1,24 @@
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return 'No available teacher';
+      }
+    }
+  }
+}
+
+export default Subjects;
 
