82084b3953fad6c85da03c0328b6c29bde9667ba
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-07-08 21:01:08 +0300

    Update Cpp.ts

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,1 +1,25 @@
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  // Declaration merging: add new property to Teacher
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingC > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return 'No available teacher';
+      }
+    }
+  }
+}
+
+export default Subjects;
 
