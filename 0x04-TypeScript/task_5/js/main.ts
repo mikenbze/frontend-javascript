b7b2dd4398657891a1ff67e1cb018de2a60cb29c
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-07-08 21:18:17 +0300

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,1 +1,34 @@
+/**
+ * Brand convention & Nominal typing
+ */
+
+export interface MajorCredits {
+  credits: number;
+  __brand: 'MajorCredits';
+}
+
+export interface MinorCredits {
+  credits: number;
+  __brand: 'MinorCredits';
+}
+
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MajorCredits',
+  };
+}
+
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MinorCredits',
+  };
+}
 
